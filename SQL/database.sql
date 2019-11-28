0b5fc44fe9e2dae77f06034291df60229c4b1b1a
Pelle Marcell <pelle.marcell@gmail.com>
2019-11-28 14:20:03 +0100

    hibaszures 3

--- SQL/database.sql
+++ SQL/database.sql
@@ -4,4 +4,4 @@ CREATE TABLE `kviz`.`users` (
    `id` INT NOT NULL AUTO_INCREMENT ,
    `username` VARCHAR(20) NOT NULL ,
    `password` INT(10) NOT NULL );
-   commit hiba szures
+   commit hiba szures10
