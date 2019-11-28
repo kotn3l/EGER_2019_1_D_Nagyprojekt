918c8cdf37c308a1955c603836b85715c10b7a76
Crix96 <marci2596>
2019-11-28 12:39:09 +0100

    adatbázis alapkonfiguráció beállítás

---
+++ protected/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASE_URL','localhost/wwww/lab2//');
+
+define('BASE_DIR','./');
+define('PUBLIC_DIR',BASE_DIR.'public/');
+define('PROTECTED_DIR',BASE_DIR.'protected/');
+
+define('CONFIG',PROTECTED_DIR.'config/');
+?>
