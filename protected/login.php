8ee9682e29d0ed24bbeff8c4d51529fc35947404
Crix96 <marci2596>
2019-12-04 23:45:11 +0100

    regisztrát felhasználó

--- protected/login.php
+++ protected/login.php
@@ -12,3 +12,16 @@
      $result = mysqli_query($db,$sql);
      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
      $active = $row['active'];
+     $count = mysqli_num_rows($result);
+
+      // If result matched $myusername and $mypassword, table row must be 1 row
+
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
