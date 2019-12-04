c6606d802ed35314561314ce43df733f1d6b7e44
bokodimate <bokodimate@gmail.com>
2019-12-04 23:19:45 +0100

    Biztonsági rés javítása

--- AFP Html/assets/protected/routing.php
+++ AFP Html/assets/protected/routing.php
@@ -3,17 +3,22 @@
 $_GET['P'] = (!array_key_exists('P', $_GET)) ? "home" : $_GET['P'];	
 	if(IsUserLoggedIn()) {
 		switch ($_GET['P']) {
+			case 'admin': 	if (IsUserAdmin()) {
+								require_once PROTECTED_ADMIN_DIR.'index.php'; 
+							} else {
+								require_once PROTECTED_DIR.'index.php';
+							} break;
 			case 'logout': UserLogout(); break;
 			case 'home': require_once PROTECTED_DIR.'index.php'; break;
 			case 'game': require_once PROTECTED_DIR.'game.html'; break;
-			case 'admin': require_once PROTECTED_ADMIN_DIR.'index.php'; break;
+			
 			default: require_once PROTECTED_DIR.'index.php'; break;
 		}
 		
 	} else {	
 		switch ($_GET['P']) {
-			case 'register': require_once PROTECTED_ADMIN_DIR.'register.php'; break;
-			case 'login': require_once PROTECTED_ADMIN_DIR.'login.php'; break;
+			case 'register': require_once PROTECTED_DIR.'register.php'; break;
+			case 'login': require_once PROTECTED_DIR.'login.php'; break;
 			default: require_once PROTECTED_DIR.'index.php'; break;
 	}
 }
