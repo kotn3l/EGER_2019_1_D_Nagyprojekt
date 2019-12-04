4579c959d56e208060d3002e147fd311b6e48fdc
Crix96 <marci2596>
2019-12-04 23:21:44 +0100

    login fajl parametrek felvezetese

---
+++ protected/login.php
@@ -0,0 +1,9 @@
+<?php
+   include("config.php");
+   session_start();
+
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form
+
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
