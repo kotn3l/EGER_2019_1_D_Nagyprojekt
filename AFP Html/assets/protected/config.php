5f902c1128783ac872a45fe1671631b2a230cdeb
bokodimate <bokodimate@gmail.com>
2019-12-04 23:11:59 +0100

    Config elkészítése

---
+++ AFP Html/assets/protected/config.php
@@ -0,0 +1,14 @@
+<?php
+define('BASE_DIR','./');
+define('PUBLIC_DIR',BASE_DIR.'public/');
+define('PROTECTED_DIR',BASE_DIR.'protected/');
+define('PROTECTED_ADMIN_DIR', PROTECTED_DIR.'admin/');
+
+define('ADMIN_BASE_URL', 'http://localhost/EGER_2019_1_D_Nagyprojekt/AFP Html/assets');
+
+define('DB_TYPE','mysql');
+define('DB_HOST','localhost');
+define('DB_NAME','labor');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_CHARSET', 'utf8');
