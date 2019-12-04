d766516952a38e7cf1f7603b8544d93d12473273
Pelle Marcell <pelle.marcell@gmail.com>
2019-12-05 00:57:31 +0100

    admin kijelentkezés hozzáadva

--- protected/login/logout.php
+++ protected/login/logout.php
@@ -4,4 +4,10 @@
    if(session_destroy()) {
       header("Location: login.php");
    }
+   if( (!isset($_SESSION["id"]) and !isset($_POST["adminChoice"])) or $_POST["adminChoice"] == "Logout")
+{
+	session_destroy();
+       header("Location: adminlogin.php");
+       exit;
+}
 ?>
