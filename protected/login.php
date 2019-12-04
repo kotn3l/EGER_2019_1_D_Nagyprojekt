c607101a1a537a9aa9e090ecee90a646da32d3f0
Crix96 <marci2596>
2019-12-04 23:23:46 +0100

    user aktiválás

--- protected/login.php
+++ protected/login.php
@@ -6,4 +6,9 @@
       // username and password sent from form
 
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+     $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+     $result = mysqli_query($db,$sql);
+     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+     $active = $row['active'];
