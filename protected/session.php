23de263f4a026d6326bf1773441ee1a5302b4cef
Pelle Marcell <pelle.marcell@gmail.com>
2019-12-05 00:27:39 +0100

    felhasználó bejelntezés megerősítése

---
+++ protected/session.php
@@ -0,0 +1,7 @@
+<?php
+   include('config.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
