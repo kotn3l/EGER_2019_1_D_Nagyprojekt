63b2aada47203f13715d3bcacd56e043ca9204c5
Pelle Marcell <pelle.marcell@gmail.com>
2019-12-05 11:46:34 +0100

    admin url hozzaadása

--- protected/config.php
+++ protected/config.php
@@ -4,4 +4,6 @@ define('BASE_URL','localhost/wwww/lab2//');
 define('BASE_DIR','./');
 define('PUBLIC_DIR',BASE_DIR.'public/');
 define('PROTECTED_DIR',BASE_DIR.'protected/');
+define('PROTECTED_ADMIN_DIR', PROTECTED_DIR.'admin/');
+
 ?>
