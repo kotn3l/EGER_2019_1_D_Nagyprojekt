2c3365df49f95284bf19803aed978f906e4b4507
Pelle Marcell <pelle.marcell@gmail.com>
2019-12-05 11:57:27 +0100

    user törlés feltétel hozzáadva

--- protected/userdelete.php
+++ protected/userdelete.php
@@ -11,4 +11,14 @@
 
            $emp_id = $_POST['user_id'];
 
-           
+           $sql = "DELETE FROM users WHERE user_id= $user_id" ;
+           mysql_select_db('kviz');
+           $retval = mysql_query( $sql, $conn );
+
+           if(! $retval ) {
+              die('Could not delete data: ' . mysql_error());
+           }
+
+           echo "Deleted data successfully\n";
+
+           mysql_close($conn);
