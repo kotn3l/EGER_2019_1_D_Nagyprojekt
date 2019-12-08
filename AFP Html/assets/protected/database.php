9a439cb30bfbe31428968e9caef84da50b72a6a6
Kornel Mirkovszki <otiwir99@yahoo.com>
2019-12-08 17:43:10 +0100

    database.php - DB_PORT használata

--- AFP Html/assets/protected/database.php
+++ AFP Html/assets/protected/database.php
@@ -6,7 +6,7 @@
 */ 
 
 function getConnection() {
-    $connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
+    $connection = new PDO(DB_TYPE.':host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
     $connection->exec("SET NAMES '".DB_CHARSET."'");
     return $connection;
 }
